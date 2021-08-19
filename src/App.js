bf437e4022800045dad87dbd6aa8c0fb64a92d28
varunsinha89 <varun.hzb97@gmail.com>
2021-08-19 18:43:44 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import {Button} from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ecomm dashboard</h1>
+
+      <button>normal button</button>
+      <Button>bootstrap button</Button>
+    </div>
+  );
+}
+
+export default App;
